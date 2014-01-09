555c72fe2e959cce1eab7494e282b3b278d21905
Edwin Gutierrez <edwin.gutierrez91@outlook.com>
2014-01-09 15:25:32 -0500

    Commit Update

--- unidad-1/lab10/js/lab10.js
+++ unidad-1/lab10/js/lab10.js
@@ -1,4 +1,10 @@
-
+//Dividir un array
 $(document).ready(function(){
-	
+	var members = [34, 35, 22, 12234, 344, 56];
+
+	$('.allArray').html(members.join('<br/>'));
+	memberssecond = members.splice(0,3);
+
+	$('.firstArray').html(memberssecond.join('<br/>'));
+	$('.secondArray').html(members.join('<br/>'));
 });
