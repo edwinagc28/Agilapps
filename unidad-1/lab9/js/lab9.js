2551ebcdcd206efdc7ec246390d386f33b4325a5
Edwin Gutierrez <edwin.gutierrez91@outlook.com>
2014-01-09 15:04:44 -0500

    Last Update

--- unidad-1/lab9/js/lab9.js
+++ unidad-1/lab9/js/lab9.js
@@ -1,4 +1,11 @@
-
+//Array de Filtrado para mostrar solo los datos deseados
 $(document).ready(function(){
-	
+	var members = ["Giovanny" ,"Edwin", "Daniel Alejandro","Alejandro", "Manuel"];
+
+	$('p.allmen').html(members.join("<br/>"));
+	members = $.grep(members, function(v){ //Buscar Elementos en un array 
+		return v.length >5;
+
+	});
+	$('p.selected').html(members.join("<br/>"));
 });
