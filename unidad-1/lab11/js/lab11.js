fc1fa0ac2a296b697374d873ab1a5f4193e1ced6
Edwin Gutierrez <edwin.gutierrez91@outlook.com>
2014-01-09 15:41:45 -0500

    Update commit second

--- unidad-1/lab11/js/lab11.js
+++ unidad-1/lab11/js/lab11.js
@@ -1,4 +1,12 @@
-
+// La combianción de Dos Arrays
 $(document).ready(function(){
-	
+	var membersOne = [34, 55, 56, 76, 89];
+	var membersTwo = [12234, 12233, 445, 454, 322, 122];
+
+	$('.firstArray').html(membersOne.join('<br/>'));
+	$('.secondArray').html(membersTwo.join('<br/>'));
+
+	members = membersOne.concat(membersTwo);
+
+	$('.combineArray').html(members.join('<br/>'));
 });
